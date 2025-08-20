c8c3666a95e905eb942f9067a19a5300db066613
Gonzalo Bessone <gonzalo.bessone.it@gmail.com>
2025-08-20 00:37:23 -0300

    Update and rename projects/database/insert_data.sql to projects/database/database-test-esp/insert_data.sql

---
+++ projects/database/database-test-esp/insert_data.sql
@@ -0,0 +1,7 @@
+
+-- Inserción de datos de prueba
+
+INSERT INTO Usuarios (id, nombre, apellido, email, fecha_registro) VALUES
+(1, 'Gonzalo', 'Bessone', 'gonzalo.bessone.it@gmail.com', '2025-08-19'),
+(2, 'Belen', 'Bessone', 'belenbessone@gmail.com', '2025-08-18'),
+(3, 'Silvina', 'Bessone', 'silvinabessone@gmail.com', '2025-08-17');
