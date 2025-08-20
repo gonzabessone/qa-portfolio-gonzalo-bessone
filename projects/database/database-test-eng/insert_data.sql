c7ff64edcee84e1f255df4874edf475b10d20528
Gonzalo Bessone <gonzalo.bessone.it@gmail.com>
2025-08-20 01:15:28 -0300

    Create insert_data.sql

---
+++ projects/database/database-test-eng/insert_data.sql
@@ -0,0 +1,6 @@
+-- Insert values into tables
+
+INSERT INTO Users (id, name, last_name, email, register_date) VALUES
+(1, 'Gonzalo', 'Bessone', 'gonzalo.bessone.it@gmail.com', '2025-08-19'),
+(2, 'Belen', 'Bessone', 'belenbessone@gmail.com', '2025-08-18'),
+(3, 'Silvina', 'Bessone', 'silvinabessone@gmail.com', '2025-08-17');
